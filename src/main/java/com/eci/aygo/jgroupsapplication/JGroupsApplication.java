33fb10ea51ba0747f13954c73a20c4e6087a052a
Rodrigo Gualtero <rodrigo.gualtero@ingeniumdata.com>
2024-11-01 11:22:31 -0500

    First commit project configuration

---
+++ src/main/java/com/eci/aygo/jgroupsapplication/JGroupsApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.eci.aygo.jgroupsapplication;
+
+/**
+ *
+ * @author rodri
+ */
+public class JGroupsApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
